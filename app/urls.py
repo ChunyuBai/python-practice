7754442c3eee52c95289296f88c933730a03e48d
ChunyuBai <baichunyu26@gmail.com>
2023-03-16 16:19:04 -0400

    base on class get var from url

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import Message
 urlpatterns = [
-  path('index',index)
+  #path('index',index)
+  #path('index/<str:name>/<int:age>',index)
+  path('message/<str:name>/<int:age>',Message.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
 # Create your views here.
-def index(request) :
-  name = request.GET.get('name','')
-  age = request.GET.get('age',18)
-  print (name,age)
-  return HttpResponse('hello django')
+# def index(request,name,age) :
+#   name = request.GET.get('name','')
+#   age = request.GET.get('age',18)
+#   print (name,age)
+#   return HttpResponse('hello django')
+
+class Message(View) :
+  def get(self, request, name, age) :
+    # print(dir(request))
+    # name = request.GET.get('name','')
+    # age = request.GET.get('age','18')
+    return HttpResponse('My name is %s and I am %s old' %(name,age))
