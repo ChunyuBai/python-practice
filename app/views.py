db8be128adbc96f45a55d4e582f9484220cf9ba2
ChunyuBai <baichunyu26@gmail.com>
2023-03-16 16:43:32 -0400

    create html templete and link to django

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import View
 # Create your views here.
 # def index(request,name,age) :
@@ -9,8 +8,5 @@ from django.views.generic import View
 #   return HttpResponse('hello django')
 
 class Message(View) :
-  def get(self, request, name, age) :
-    # print(dir(request))
-    # name = request.GET.get('name','')
-    # age = request.GET.get('age','18')
-    return HttpResponse('My name is %s and I am %s old' %(name,age))
+  def get(self, request, name) :
+    return render(request,'index.html',{'name':name})
