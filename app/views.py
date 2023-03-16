1f594770d5a45aa4fc8b29e5c382f0f52005c865
ChunyuBai <baichunyu26@gmail.com>
2023-03-16 15:51:31 -0400

    pass var through url"

--- app/views.py
+++ app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request) :
+  name = request.GET.get('name','')
+  age = request.GET.get('age',18)
+  print (name,age)
   return HttpResponse('hello django')
